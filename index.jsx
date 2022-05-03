12752bcf8124e732a25f61ee696ab51a36ea5301
Noman <noman99_@hotmail.com>
2022-05-03 11:42:26 +0200

    Navigate around in the app with React Router

--- index.jsx
+++ index.jsx
@@ -1,7 +1,31 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+import {Routes, Route, Link, BrowserRouter} from "react-router-dom";
+
+function FrontPage() {
+    return <div>
+    <h1>Kristiania Movie db</h1>
+        <ul>
+            <li><Link to={"/movies"}>List Movies</Link></li>
+            <li><Link to={"/movies/new"}>New Movie</Link></li>
+        </ul>
+    </div>;
+}
+
+function Application() {
+    return <BrowserRouter>
+        <Routes>
+
+            <Route path="/" element={<FrontPage/>}/>
+            <Route path="/movies/new" element={<h1>New movie</h1>}/>
+            <Route path="/movies" element={<h1>List Movies</h1>}/>
+        </Routes>
+    </BrowserRouter>
+
+
+}
 
 ReactDOM.render(
-    <h1>Kristiania Movie db</h1>,
+    <Application/>,
     document.getElementById("app")
 )
--- readme.md
+++ readme.md
@@ -1,1 +1,10 @@
-testxd
+# Movie Database
+
+* [x] Say hello to react - Parcel generates React App from jsx
+* [x] Routing requests in React - navigate
+* [] List Existing movies
+* [] Add new movie using forms and state 
+* [] Encapsulate movie database
+* [] Express app
+* [] Move API to Server
+* [] Deploy Heroku
